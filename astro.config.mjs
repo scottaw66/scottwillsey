c48c7fe05e13a2f6c8df3aa9afc6199942de45ca
Scott Willsey <sunny.flame0794@fastmail.com>
2023-09-07 14:57:49 -0700

    Adds search back in and (I think) finishes the migration to Astro 3.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://scottwillsey.com/",
-    integrations: [expressiveCode()],
+    integrations: [expressiveCode(), pagefind()],
 });
