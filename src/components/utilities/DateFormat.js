152d395089fd6b15f00d1767c60682c31977b045
Scott Willsey <sunny.flame0794@fastmail.com>
2023-12-23 16:29:56 -0800

    Fixes date handling due to breaking changes in date-fns 3.0.0+

--- src/components/utilities/DateFormat.js
+++ src/components/utilities/DateFormat.js
@@ -1,24 +1,18 @@
 // Date format utilities
 
 import { format } from "date-fns";
-import enUS from "date-fns/locale/en-US/index.js";
-import { formatInTimeZone } from "date-fns-tz";
+import { enUS } from "date-fns/locale/en-US";
 
 const postPattern = "eeee, dd MMM yyyy";
-const tz = "America/Los_Angeles";
 
 export function rfc2822(date) {
   const pattern = "eee, dd MMM yyyy HH:mm:ss zzz";
 
-  return formatInTimeZone(date, tz, pattern, { locale: enUS });
+  return format(date, pattern, { locale: enUS });
 }
 
 export function postdate(date) {
-  return formatInTimeZone(date, tz, postPattern, { locale: enUS });
-}
-
-export function utcdate(date) {
-  return formatInTimeZone(date, "UTC", postPattern, { locale: enUS });
+  return format(date, postPattern, { locale: enUS });
 }
 
 export function year() {
